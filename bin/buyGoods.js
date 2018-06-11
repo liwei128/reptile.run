581aa651f34bf1f06a441bbbeca853e56650c7b8
liwei <18576462480@163.com>
2018-06-11 17:24:27 +0800

    小米

---
+++ bin/buyGoods.js
@@ -0,0 +1,76 @@
+var fs = require('fs');
+var content = fs.read(fs.workingDirectory+'/config/mi.json');
+var data = JSON.parse(content);
+var index = 0;
+  
+var settings = {
+  operation: "GET",
+  encoding: "utf-8",
+  headers: {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36"
+  }
+}; 
+
+function setCookies(){
+    var jsonList = data.user.cookies;
+    jsonList.forEach(function(cook){  
+        cook.expires = (new Date()).getTime() + (1000 * 60 * 60 );
+        phantom.addCookie(cook);
+    });
+    phantom.cookiesEnabled = true;
+}
+
+setTimeout(function () {
+        phantom.exit();
+    }, 10000);
+
+var  page = require('webpage').create(); 
+page.settings.loadImages = false;
+page.settings.resourceTimeout = 8000;
+
+page.onAlert = function(test){
+    console.log(test);
+}
+function start(goodsUrl,select){
+
+    setCookies();
+    buyGoods(goodsUrl,select);
+}
+
+function buyGoods(goodsUrl,select){
+	var s= new Date().getTime();
+    page.open(goodsUrl,settings,function (status) { 
+        setTimeout(function(){
+             page.injectJs("./zepto.min.js",function(){
+             });
+			//选择版本颜色保障服务等	
+			for (var i=0;i<select.length;i++){
+				setTimeout(function(){
+					page.evaluate(function(i,j){
+						$(".list-wrap#J_list >div").eq(i).children("ul").children("li").eq(j).click(); 
+					},index,select[index]);
+					index++;
+				},600*i);
+			}
+            //提交购物车
+            setTimeout(function(){
+                setCookies();
+                page.evaluate(function(){
+                    $("#J_buyBtnBox>li>a").click(); 
+                }); 
+				console.log("success");
+            },600*select.length) ;
+			//退出
+			setTimeout(function(){
+               phantom.exit();
+            },600*(select.length+1)) ;
+        },600);
+		
+    });
+}
+start(data.goodsInfo.url,data.goodsInfo.params_index);
+
+
+
+
---
+++ bin/login.js
@@ -0,0 +1,61 @@
+var fs = require('fs');
+var url = "https://account.xiaomi.com/pass/serviceLogin?callback=https%3A%2F%2Forder.mi.com%2Flogin%2Fcallback%3Ffollowup%3Dhttps%253A%252F%252Fwww.mi.com%252Findex.html%26sign%3DMjM0MWU0NjBlOTU1YzY4NGQzOTc3MDk4N2M2MjQ5Y2ZiZTMxNTFlZQ%2C%2C&sid=mi_eshop&_bannerBiz=mistore&_qrsize=180";
+var content = fs.read(fs.workingDirectory+'/config/mi.json');
+var user = JSON.parse(content).user;
+
+
+var page = require('webpage').create();    
+var settings = {
+  operation: "GET",
+  encoding: "utf-8",
+  headers: {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36"
+  }
+};  
+page.settings.loadImages = true;
+page.settings.resourceTimeout = 20000; 
+window.setTimeout(function () {
+            phantom.exit();
+        }, 10000);
+page.onAlert = function(test){
+    console.log(test);
+}
+
+
+
+function login(){
+    phantom.clearCookies();
+    page.open(url,settings,function (status) { 
+        if (status == 'success') { 
+            page.injectJs("./zepto.min.js",function(){
+            });
+            page.evaluate(function(userName,password){
+                $("input[name='user']").val(userName);
+                $("input[name='password']").val(password);
+                $("input[type='submit']").click();
+            },user.userName,user.password);
+        }
+        setTimeout(function(){
+            var loginStatus = page.evaluate(function(){
+                var user = $("#J_userInfo>span.user").html();
+                if(user){
+                    return true;
+                }
+                return false;
+            });
+            if(loginStatus){
+                console.log(JSON.stringify(page.cookies));
+            }else{
+				console.log(false);
+			}
+            phantom.exit();
+        }, 5000);
+    
+    });
+    
+}
+
+login();
+
+    
